992ca057c3281c904c7ef34c081f8d468512413c
Ole Holm Frandsen <froksen@gmail.com>
2012-09-12 19:59:56 +0200

    Added About dialog

---
+++ aboutprog.cpp
@@ -0,0 +1,45 @@
+#include "aboutprog.h"
+#include "ui_aboutprog.h"
+#include <QDesktopWidget>
+
+AboutProg::AboutProg(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::AboutProg)
+{
+    ui->setupUi(this);
+
+//    QRect r = AboutProg::geometry();
+//    r.moveCenter(QApplication::desktop()->availableGeometry().center());
+
+    //Append texts
+    appendTranslators();
+    appendSpecialThanks();
+}
+
+AboutProg::~AboutProg()
+{
+    delete ui;
+}
+
+//This method makes it eaiser to add new translators, without having to retranste the entire textedit.
+void AboutProg::appendTranslators()
+{
+    ui->textEdit_2->append(trUtf8("Danish - Ole 'Froksen' Holm Frandsen"));
+    ui->textEdit_2->append(trUtf8("German - Chris 'saftsocken' Räss"));
+    ui->textEdit_2->append(trUtf8("Spanish - Javi"));
+    ui->textEdit_2->append(trUtf8("Italian - Symbian flo"));
+    ui->textEdit_2->append(trUtf8("Romanian - Mamut R. Ghiunhan (aka V3n3RiX)"));
+
+}
+
+void AboutProg::appendSpecialThanks()
+{
+    ui->textEditThanks->append("3electrons");
+    ui->textEditThanks->append("kalac");
+    ui->textEditThanks->append("swiftscythe");
+    ui->textEditThanks->append("jboss1995");
+    ui->textEditThanks->append("undefined");
+    ui->textEditThanks->append("symbianflo");
+    ui->textEditThanks->append(trUtf8("Mamut R. Ghiunhan (aka V3n3RiX)"));
+
+}
